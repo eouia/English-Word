173f69445d617ad55f1cd6324979c1174591229d
eouia <eouia0819@gmail.com>
2026-05-19 12:47:58 +0800

    Set Quartz base URL

--- site/quartz.config.ts
+++ site/quartz.config.ts
@@ -14,6 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "ko-KR",
+    baseUrl: "eouia.github.io/English-Word",
     ignorePatterns: [
       "site",
       "publish",
